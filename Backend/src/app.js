a94ee28fe1aa84037cfd474b81c40f9091d277e6
sumit saini <sumitsaini@sumits-MacBook-Air.local>
2026-08-13 23:56:09 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,18 +9,20 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://ai-interview-platform-nine-pied.vercel.app"
+    ],
     credentials: true
 }))
 
-/* require all the routes here */
+// require all the routes here
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
-/* using all the routes here */
+// using all the routes here
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-
-module.exports = app 
+module.exports = app
