dc62f24ee95cf9c193587d023a0e5d7ad03f59f7
sumit saini <sumitsaini@sumits-MacBook-Air.local>
2026-08-13 23:18:24 +0530

    Configure server port for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,11 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB().then(() => {
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000")
+
+    const PORT = process.env.PORT || 3000
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
     })
+
 })
